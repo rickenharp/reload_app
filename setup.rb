3de7fe83dba2c62f9ef632641aa3ad7822dd7d8c
Alexander Gräfe <alexander.graefe@googlemail.com>
2021-07-11 17:36:48 +0200

    Force reload of boot files

--- setup.rb
+++ setup.rb
@@ -6,5 +6,8 @@ require "zeitwerk"
 ::Loader.logger = method(:puts)
 ::Loader.push_dir(File.join(__dir__, 'system'))
 ::Loader.push_dir(File.join(__dir__, 'lib'))
+::Loader.on_load('Container') do |_foo|
+  $LOADED_FEATURES.reject! { |path| path =~ %r{/system/boot/} }
+end
 ::Loader.enable_reloading
 ::Loader.setup
